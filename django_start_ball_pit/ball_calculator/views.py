bde48235ecae2cb3994a34ce72290fc2113043ff
mcriqui <maggie.criqui@gmail.com>
2015-07-16 20:25:01 -0400

    created view

--- django_start_ball_pit/ball_calculator/views.py
+++ django_start_ball_pit/ball_calculator/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django import forms
+from ball_calculator import Widths, Lengths
 
 # Create your views here.
+class SelectSizeForm(forms.Form):
+    lengths = forms.ModelChoiceField(queryset=Ranking.objects.all(), empty_label=None)
+    widths = forms.ModelChoiceField(queryset=Ranking.objects.all(), empty_label=None)
+
+def get_template(request):
+    form = SelectSizeForm(request.Get)
+    volume = None
+
+    if form.is_vald():
+        length = form.cleaned_data['length']
+        width = form.cleaned.data['width']
